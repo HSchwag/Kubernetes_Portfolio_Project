07f2e7d209a39f30b1ca8fd74b0c523a8eeb63b1
HSchwag <hunterschwager@gmail.com>
2025-09-30 15:01:30 -0600

    web app running good enough, ready for docker

--- core/urls.py
+++ core/urls.py
@@ -6,7 +6,7 @@ urlpatterns = [
     path('', home, name='home'),
     path('signup/', signup, name='signup'),
     path('login/',  auth_views.LoginView.as_view(template_name="login.html"), name='login'),
-    path('logout/', auth_views.LogoutView.as_view(template_name="logout.html"), name='logout'),
+    path('logout/', auth_views.LogoutView.as_view(next_page="home"), name='logout'),
     path('dashboard/', dashboard, name='dashboard'),
     path("healthz/", healthz, name="healthz"),
     path('metrics/', metrics, name='metrics'),
--- core/views.py
+++ core/views.py
@@ -1,5 +1,7 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.http import HttpResponse
+from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth import login as auth_login
 
 # Create your views here.
 
@@ -7,7 +9,15 @@ def home(request):
     return render(request, 'home.html')
 
 def signup(request):
-    return render(request, 'signup.html')
+    if request.method == 'POST':
+        form = UserCreationForm(request.POST)
+        if form.is_valid():
+            user = form.save()
+            auth_login(request, user)
+            return redirect('dashboard')
+    else:
+        form = UserCreationForm()
+    return render(request, 'signup.html', {"form": form})
 
 def login(request):
     return render(request, 'login.html')
